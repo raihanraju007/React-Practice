53f34b6b6c59e3695511025fb1db984a33083630
raihanraju007 <raihanraju007@gmail.com>
2023-02-05 15:35:13 +0600

    Props and destructuring

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Card from "./componant/Card";
 function App(){
     return <div>
                <h1 className="headingStyle">Todo App</h1>
-               <Card />
-               <Card />
-               <Card />
+               <Card todoTitle = "Call Mother" description = "1 In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. " />
+               
+                <Card todoTitle = "Call Father" description = "2 In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. " /> 
+
+                <Card todoTitle = "Call Brother" description = "3 In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. " />
+               
         </div>
 }
 
--- src/componant/Card.js
+++ src/componant/Card.js
@@ -1,14 +1,17 @@
-const todoTitle = "Call Family";
-const description = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. "
+// const todoTitle = "Call Family";
+// const description = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. "
 const date = new Date();
 const cdate = date.getDate();
 const cmonth = date.getMonth();
 const cyear = date.getFullYear();
 
-function Card(){
+function Card(props){
+  console.log(props);
+ const {description}= props;
     return     <div>
       <div className="card">
-        <h3 className="cardTitle">{todoTitle}</h3>
+        <h3 className="cardTitle">{props.todoTitle}</h3>
+        {/* <p className="cardDesc">{props.description}</p> */}
         <p className="cardDesc">{description}</p>
         <p className="cardFooter">{cdate + "/" + cmonth + "/" + cyear}</p>
       </div>
