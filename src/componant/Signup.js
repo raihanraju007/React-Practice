69c1a600b85e4546b685b9fa8b0370f2ef6c99b0
raihanraju007 <raihanraju007@gmail.com>
2023-06-08 16:55:06 +0600

    validate form data using yup with formik1

--- src/componant/Signup.js
+++ src/componant/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Formik, useFormik } from 'formik';
+import { Form, Formik, useFormik } from 'formik';
+import * as yup from 'yup';
 
 function Signup() {
 
@@ -9,11 +10,17 @@ const Formik = useFormik({
         email: '',
         password: ''
     },
+    validationSchema: yup.object({
+        name: yup.string().min(2, "Name must have at least 2 characters").required(),
+        email: yup.string().email().required(),
+        password: yup.string().min(6,"Password have at least 6 characters").required() ,
+    }),
     onSubmit: (values, {resetForm}) => {
         console.log(values);
         resetForm({values: ""})
     }
 })
+// console.error(Formik.errors);
 
   return (
     <div>
@@ -22,14 +29,20 @@ const Formik = useFormik({
         <div>
             <label htmlFor='name'>Name : </label>
             <input type='text' id='name' name='name' value={Formik.values.name} onChange={Formik.handleChange}/>
+            <br/>
+            {Formik.touched.name && Formik.errors.name && <span style={{color:'red'}}>{Formik.errors.name}</span>}
         </div>
         <div>
             <label htmlFor='email'>Email : </label>
             <input type='text' id='email' name='email' value={Formik.values.email} onChange={Formik.handleChange}/>
+            <br/>
+            {Formik.touched.email && Formik.errors.email && <span style={{color:'red'}}>{Formik.errors.email}</span>}
         </div>
         <div>
             <label htmlFor='password'>Password : </label>
             <input type='text' id='password' name='password' value={Formik.values.password} onChange={Formik.handleChange}/>
+            <br/>
+            {Formik.touched.password && Formik.errors.password && <span style={{color:'red'}}>{Formik.errors.password}</span>}
         </div>
         <button type='submit'>Signup</button>
       </form>
