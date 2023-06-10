0c336ccd7b3bd3a8711fa96ba7c3fd337c4d3727
raihanraju007 <raihanraju007@gmail.com>
2023-06-10 12:12:44 +0600

    fetching data with useEffect hook

--- src/App.js
+++ src/App.js
@@ -15,12 +15,14 @@ import Child from "./componant/SATET_LIFTING/Child";
 import Signup from "./componant/Signup";
 import Toggle from "./componant/Toggle/Toggle";
 import FAQS from "./componant/FAQ/FAQS";
-import UseEffectExample from "./componant/HOOKS/useEffect/UseEffectExample";
+// import UseEffectExample from "./componant/HOOKS/useEffect/UseEffectExample";
+import DataFetch from "./componant/HOOKS/useEffect/DataFetch";
+
 
 function App() {
   return (
     <div>
-      <UseEffectExample />
+        <DataFetch />
     </div>
   );
 }
