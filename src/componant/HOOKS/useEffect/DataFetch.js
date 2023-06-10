2c4a10a0a8f2966fd5b5946682d6871318b28b19
raihanraju007 <raihanraju007@gmail.com>
2023-06-10 12:33:42 +0600

    fetching data with useEffect hook

--- src/componant/HOOKS/useEffect/DataFetch.js
+++ src/componant/HOOKS/useEffect/DataFetch.js
@@ -1,24 +1,46 @@
 import React, { useEffect, useState } from 'react'
 
+const loadingMessage = <p>todos is loading</p>
+
 const DataFetch = () => {
 const[todos, setTodos] = useState(null)
+const[isLoading, SetIsLoading] = useState(true)
+const[error, setError] = useState(null)
 
   useEffect(() =>{
     fetch("https://jsonplaceholder.typicode.com/todos")
     .then((res)=>{
-      return res.json();
+      if(!res.ok){
+        throw Error("Fetching is not successful");
+      }else{
+        return res.json();
+      }
+      
     })
     .then((data)=>{
       setTodos(data);
+      SetIsLoading(false);
+      setError(null);
+    })
+    .catch((error) =>{
+      setError(error.message)
+      SetIsLoading(false);
     })
   },[]);
+
+  const todosElement = todos && 
+           todos.map((todo) => {
+      return <p key={todo.id}>{todo.title}</p>
+    })
+
+
 
   return (
     <div>
       <h1>Todos</h1>
-      {todos && todos.map((todo) => {
-          return <p key={todo.id}>{todo.title}</p>
-        })}
+      {error && <p>{error}</p>}
+      {isLoading && loadingMessage}
+      {todosElement}
     </div>
   )
 }
