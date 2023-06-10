49dedde58c03602441f42b95476e950e102f7eb5
raihanraju007 <raihanraju007@gmail.com>
2023-06-10 11:07:09 +0600

    React Hooks | useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
 import Card from "./componant/Card";
-import Data from "./data.json"
+import Data from "./data.json";
 import Card2 from "./componant/Card2";
 import ReactBootstrap from "./React_Bootstrap/ReactBootstrap";
 import STATE from "./STATE";
 import ConditionalRendering from "./CONDITIONAL_RENDERING/ConditionalRendering";
 import EventHandlerClass from "./event_handler_class/index";
 import EvenBinding from "./even_binding/EvenBinding";
-import ClassCompIndex from "./Hooks_UseState/ClassCompIndex";
-import FuncCompIndex from "./Hooks_UseState/FuncCompIndex";
-import UpdateFunComponent from "./Hooks_UseState/UpdateFunComponent";
+import ClassCompIndex from "./componant/HOOKS/Hooks_UseState/ClassCompIndex";
+import FuncCompIndex from "./componant/HOOKS/Hooks_UseState/FuncCompIndex";
+import UpdateFunComponent from "./componant/HOOKS/Hooks_UseState/UpdateFunComponent";
 import Form from "./componant/Form/Form";
 import Child from "./componant/SATET_LIFTING/Child";
 import Signup from "./componant/Signup";
 import Toggle from "./componant/Toggle/Toggle";
 import FAQS from "./componant/FAQ/FAQS";
+import UseEffectExample from "./componant/HOOKS/useEffect/UseEffectExample";
 
-
-function App(){
-    return <div>
-            <FAQS />
-        </div>
+function App() {
+  return (
+    <div>
+      <UseEffectExample />
+    </div>
+  );
 }
+
 export default App;
