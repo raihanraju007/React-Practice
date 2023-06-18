3efe43b50dd4d8e7489a70ea8da93e67f822bd67
raihanraju007 <raihanraju007@gmail.com>
2023-06-18 16:10:50 +0600

    useContext Hook

--- src/componant/PROP_DRILLING/Component1.js
+++ src/componant/PROP_DRILLING/Component1.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import Component2 from './Component2'
-
+import { UserContext } from './UserContext'
 const Component1 = () => {
     const [user, setUser] = useState({id: 101, name: "Raihan"})
+    const [text, setText] = useState("Hello I Am Text")
   return (
-    <div>
-        <Component2 user={user}/>
-    </div>
+    <UserContext.Provider value={{user, text}}>
+        <Component2 />
+    </UserContext.Provider>
   )
 }
 
--- src/componant/PROP_DRILLING/Component4.js
+++ src/componant/PROP_DRILLING/Component4.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext'
 
-const Component4 = ({user}) => {
+
+const Component4 = () => {
+  const {user,text} = useContext(UserContext);
+
   return (
     <div>
       <h3>{user.id}</h3>
       <h3>{user.name}</h3>
+      <h3>{text}</h3>
     </div>
   )
 }
